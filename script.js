4bea86ed39b76224e1a91e0948813261d065f7b1
Jay <138824448+jaythemself@users.noreply.github.com>
2023-08-06 23:02:32 -0500

    none of my previous commits are showing up on github??? Changes that have occurred: wrote and rewrote every function in script and scriptHelper probably half a dozen times. Got planetary info to display for user but changing that broke preventDefault and now my npm tests are reversing expectations so I have to rewrite validateInput and formSubmission again in all likelihood. Added book instructions for easy reference. Changed value names multiple times, then went through and (hopefully) finally standardized them. my brain is fried i'm so mad my prev commits aren't showing up. this is spectrum's fault i hate this new modem. Hi sorry anyone who's actually reading this I've been at this for 12 hours straight.

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 // Write your JavaScript code here!
 
 window.addEventListener("load", function() {
+ //putting together helper.js functions. listed planets uses myFetch to access provided api of full list, then selects one at random with pickPlanet
+ let listedPlanets;
+ let listedPlanetsResponse = myFetch();
+ listedPlanetsResponse.then(function (result) {
+     listedPlanets = result;
+     console.log(listedPlanets);
+     let selectedPlanet = pickPlanet(listedPlanets);
+     console.log(selectedPlanet);
+     addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image);
+ });
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+ let submitButton = document.getElementById("formSubmission");
+ //preventing page from automatically reloading after input, setting up values to use when invoking formSubmission dot notation line 27
+ //still unsure when/where i should be using const vs let? if i use const will user's later attempts not be ignored??
+ submitButton.addEventListener("click", function (event) {
+     event.preventDefault();
+     let pilotInput = document.getElementById("pilotName");
+     let copilotInput = document.getElementById("copilotName");
+     let fuelInput = document.getElementById("fuelLevel");
+     let cargoInput = document.getElementById("cargoMass");
+
+     let list = document.getElementById("faultyItems");
+
+     formSubmission(document, list, pilotInput.value, copilotInput.value, fuelInput.value, cargoInput.value);
+ });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,114 +1,94 @@
-// Write your helper functions here!
+/*PROBLEMS: preventDefault() not working now and hitting submit refreshes page, 
+validate input under formSubmission is reversing with tests (expects "Fuel too low!" when fuel is high enough, etc?? was working fine earlier), 
+user input not displaying/colors not changing (issue with visibility? nothing shows up on user end at all. stopped working when i got planet info to display finally)*/
+
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+  //UX display of randomly selected planet used in script.js event handler (listed planets section)
+  let missionTarget = document.getElementById("missionTarget");
+  missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+               <ol>
+                   <li>Name: ${name}</li>
+                   <li>Diameter: ${diameter}</li>
+                   <li>Star: ${star}</li>
+                   <li>Distance from Earth: ${distance} </li>
+                   <li>Number of Moons: ${moons}</li>
+               </ol>
+               <img src="${imageUrl}">
+               `;
 }
 
 function validateInput(testInput) {
-   /*validate if user forgot to enter a value in any field. add alert to notify user all fields required
-   make sure info is valid
-   should return "Empty", "Not a Number", "Is a Number"*/
-   /*if (testInput === "") {
-    return "Empty";
-   } else if (!isNaN(testInput)) {
-    return "Is a Number";
-   } else {
-    return "Not a Number";
-   }*/
-   let numInput = Number(testInput);
-
+ //testing to make sure user input isn't empty/not filled out and that appropriate fields are a string or a number
    if (testInput === "") {
     return "Empty";
-   } else if (isNaN(numInput)) {
+   } else if (isNaN(testInput) === true) {
     return "Not a Number";
-   } else if (isNaN(numInput) === false) {
+   } else if (isNaN(testInput) === false) {
     return "Is a Number";
    }
-   /*by wrapping numInput in Number() isn't it guaranteed that Not a Number will never get triggered, even if user inputs 
-   a string? Or would Number(string) come back as undefined? Idr.
-   Check back over prev work in replit to refresh memory on how I validated input then, if still doesn't click talk w TA*/
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   /*will take a document parameter and strings rep the 4 reqs. make sure to call in script
-   cannot use == or === to check NaN. use built in isNaN(value) returns true/false
-   double check and make sure instructions don't care where alert is addressed*/
+function formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass) {
+ //creating variables for innerHTML dot notation and sending up alerts if validateInput results were unwanted
   let pilotStatus = document.getElementById("pilotStatus");
   let copilotStatus = document.getElementById("copilotStatus");
-  let fuel = document.getElementById("fuelStatus");
-  let cargo = document.getElementById("cargoStatus");
-
-  if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuel) === "Empty" || validateInput(cargo) === "Empty") {
+  let fuelStatus = document.getElementById("fuelStatus");
+  let cargoStatus = document.getElementById("cargoStatus");
+  let launchStatus = document.getElementById("launchStatus");
+  
+  if (validateInput(pilotName) === "Empty" || validateInput(copilotName) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoMass) === "Empty") {
     alert("All fields of the form must be filled out.");
-    /*ik brain is fried rn but there has to be a better way to write this ^^^^. might need to expand it and iterate through instead of trying to 
-    write as little code as possible, sometimes less does not mean most concise
-    Yeah getting rid of array of valid submission idk where I was going with this i can barely make sense of it and it's been like. a day and a half.
-    Just rewrite this whole section more in line w prev validations. maybe rewrite below too it'll be annoying taking leftover scraps and fitting
-    it to new stuff. keeping track of variable names my beloathed. Don't code w a migraine kids*/
-   } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuel) === "Not a Number" || validateInput(cargo) === "Not a Number") {
-    alert("Provided information is invalid, please check your input.");
-   } else {
     list.style.visibility = "visible";
-    pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
-    copilotStatus.innerHTML = `Co-pilot ${copilot} is readt for launch`;
-    let launchStatus = document.getElementById("launchStatus");
-
-    /*add function to check values after checking validity? makes most sense to put it there but check instructions. reference prev GA, #3 i think*/
+   } else if (validateInput(pilotName) === "Is a Number" || validateInput(copilotName) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoMass) === "Not a Number") {
+    alert("Provided information is invalid, please check your input.");
+    list.style.visibility = "visible";
+   } else {
+    pilotStatus.innerHTML = `Pilot ${pilotName} is ready for launch`;
+    copilotStatus.innerHTML = `Co-pilot ${copilotName} is ready for launch`;
    }
-   
-   /*
-   Do i need to be more specific for Inputs/use key words? double check instructions <--instructions do not specify. maybe check grading specs to 
-   be extra safe?
-   let fuelInput;
-   if (fuelLevel >= 10000) {
-    fuelInput = "acceptable fuel levels";
-   } else {
-    fuelInput = "fuel too low";
-   };
-
-   let cargoInput;
-   if (cargoMass <= 10000) {
-    cargoInput = "acceptable cargo weight";
-   } else {
-    cargoInput = "cargo too heavy";
-   };
-
-   write document portion (let xStatus = document.getElementById i think? double check dot notation and finish validating/user feedback. 
-   also innerHTML*/
+   //adjusting innerHTML for specific feedback on what is wrong, adjusting display of results for user
+      if (fuelLevel < 10000 && cargoMass < 10000) {
+         list.style.visibility = "visible";
+         fuelStatus.innerHTML = "Fuel level too low for launch";
+         cargoStatus.innerHTML = "Cargo mass low enough for launch";
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+         launchStatus.style.color = "rgb(199, 37, 78)";
+      } else if (fuelLevel > 10000 && cargoMass > 10000) {
+         list.style.visibility = "visible";
+         fuelStatus.innerHTML = "Fuel level high enough for launch";
+         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+         launchStatus.style.color = "rgb(199, 37, 78)";
+      } else if (fuelLevel < 10000 && cargoMass > 10000) {
+         list.style.visibility = "visible";
+         fuelStatus.innerHTML = "Fuel level too low for launch";
+         cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+         launchStatus.style.color = "rgb(199, 37, 78)";
+      } else if (fuelLevel > 10000 && cargoMass < 10000) {
+         list.style.visibility = "visible";
+         fuelStatus.innerHTML = "Fuel level high enough for launch";
+         cargoStatus.innerHTML = "Cargo mass low enough for launch";
+         launchStatus.innerHTML = "Shuttle is Ready for Launch";
+         launchStatus.style.color = "rgb(65, 159, 106)";
+      }
 }
 
-/*Rest of book instructions for this section:
-update the li elements pilotStatus and copilotStatus to include the pilot's name and the co-pilot's name.
-If the user submits a fuel level that is too low (less than 10,000 liters), change faultyItems to visible with an updated fuel status stating that 
-    there is not enough fuel for the journey. The text of the h2 element, launchStatus, should also change to "Shuttle not ready for launch" and the 
-    color should change to red.
-If the user submits a cargo mass that is too large (more than 10,000 kilograms), change the list to visible with an updated cargo status stating 
-    that there is too much mass for the shuttle to take off. The text of launchStatus should also change to "Shuttle not ready for launch" and 
-    the color should change to a particular shade of red, #C7254E
-If the shuttle is ready to launch, change the text of launchStatus to a particular shade of green #419F6A and display "Shuttle is ready for launch"*/
-
 async function myFetch() {
-    let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
-    return planetsReturned;
+  //fetching planet list from api provided in read me
+  let planetsReturned;
+  planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+      return response.json();
+      });
+  return planetsReturned;
 }
 
 function pickPlanet(planets) {
+ //selecting a random planet from provided planet list
+   return planets[Math.floor(Math.random() * planets.length)];
 }
 
 module.exports.addDestinationInfo = addDestinationInfo;
